89ab89803a5565c925d2319873f4b68351aa4c8c
Henrique <Henrique@HENRIQUE-PC>
2020-09-07 18:55:15 -0300

    Add project files.

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class ProdutoDAOEntity : IProdutoDAO, IDisposable
+    {
+        private LojaContext context;
+        public void Adicionar(Produto p)
+        {
+            context.Produtos.Add(p);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(Produto p)
+        {
+            context.Produtos.Update(p);
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            return context.Produtos.ToList();
+        }
+
+        public void Remover(Produto p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        //private void LockOperation(Action<Produto> operation, Produto p)
+        //{
+        //    operation(p);
+        //    context.SaveChanges();
+        //}
+
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            //RecuperarUsandoEntity();
+            //RemoverUsandoEntity();
+            //RecuperarUsandoEntity();
+        }
+
+        private static void RecuperarUsandoEntity()
+        {
+            using (var context = new LojaContext())
+            {
+                var produto = context.Produtos.First();
+                Console.WriteLine("Produto encontrado => " + produto.Nome);
+            }
+        }
+        private static void RemoverUsandoEntity()
+        {
+            using (var context = new LojaContext())
+            {
+                context.Produtos.RemoveRange(context.Produtos);
+                context.SaveChanges();
+            }
+        }
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var context = new LojaContext())
+            {
+                context.Produtos.Add(p);
+                context.SaveChanges();
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
